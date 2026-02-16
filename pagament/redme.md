886781ae65a9595e4a5b20a7e20450b32584bc86
João Gabriel De Jesus Cunha <141980608+jogabrieldev@users.noreply.github.com>
2026-02-16 13:35:32 -0300

    Commit-inicial
    Primeiro commit do projeto de pagamento que vou utilizando uma ferramenta gratuita, para aprendizado e desenvolvimento meu. Nesse commit foi criado a estrutura inicial do projeto e instalado algumas ferramentas que vão ser usadas no projeto

---
+++ pagament/redme.md
@@ -0,0 +1,2 @@
+## PROJETO PAGAMENT
+
---
+++ pagament/src/server.ts
@@ -0,0 +1,18 @@
+// src/server.ts
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT= process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('API de Pagamentos Ativa! 🚀');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
